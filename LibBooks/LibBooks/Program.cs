d53964127972c32c5d3787c6f62c3100d7fa595f
IM94 <gladiator.pw@gmail.com>
2023-08-20 22:26:53 +0300

    Added list for books and authors

--- LibBooks/LibBooks/Program.cs
+++ LibBooks/LibBooks/Program.cs
@@ -1,27 +1,63 @@
 ﻿using System;
 using System.Xml.Linq;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace LibBooks
 {
     class Program
     {
+        string path = @"./author_books.json";
+        static List<string> books = new List<string>(); // the list of the books
+        static List<string> authors = new List<string>();   // the list of the authors
 
-        public static void Main()
+        static void Main()
         {
-            Program library = new Program();  // Creating a instance of the LibBooks class
+            Program library = new Program(); // creating instance of the Program class
 
-            string nameBook = library.WriteBook();    // Write a book name
-            string nameAuthor = library.WriteAuthor();    // Write a author name
+            while (true) // the loop for working with "library"
+            {
+                Console.WriteLine("Choose an action:");
+                Console.WriteLine("1. Read List");
+                Console.WriteLine("2. Write Book");
+                Console.WriteLine("3. Write Author");
+                Console.WriteLine("4. Exit");
 
-            library.ReadList(nameBook, nameAuthor); // Read the books and author names
-            Console.ReadLine();
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        library.ReadList();
+                        break;
+
+                    case "2":
+                        string book = library.WriteBook();
+                        books.Add(book); // add the book to the list
+                        break;
+
+                    case "3":
+                        string author = library.WriteAuthor();
+                        authors.Add(author); // add the author to the list
+                        break;
+
+                    case "4":
+                        Environment.Exit(0); // Exit to program
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
         }
 
-        public void ReadList(string name, string author)
+        public void ReadList()
         {
+            Console.WriteLine("Books:");
+            Console.WriteLine(string.Join(", ", books)); // write books
 
-            Console.WriteLine(name);
-            Console.WriteLine(author);
+            Console.WriteLine("Authors:");
+            Console.WriteLine(string.Join(", ", authors)); // write authors
         }
 
         public string WriteBook()
